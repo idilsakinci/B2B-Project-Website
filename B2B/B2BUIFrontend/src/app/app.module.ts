89cd18b5e5432b38547575a77d4dca1b8de108e2
İdil Sakıncı <76444215+idilsakinci@users.noreply.github.com>
2023-06-14 18:05:12 +0300

    folder added

---
+++ B2B/B2BUIFrontend/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import { AuthInterceptor } from './ui/login/interceptor/auth.interceptor';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    SweetAlert2Module.forRoot(),
+    UiModule,
+    HttpClientModule,
+  ],
+  providers: [
+    { provide: 'apiUrl', useValue: 'https://localhost:7146/api/'},
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
